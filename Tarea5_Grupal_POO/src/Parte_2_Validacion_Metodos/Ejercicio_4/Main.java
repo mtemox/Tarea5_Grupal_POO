ca56e1633b55ea860113f3a56a7b706b2596224d
HanlysNat <137083744+HanlysNat@users.noreply.github.com>
2025-05-07 22:46:22 -0500

    Henry/Hanly Commit

--- Tarea5_Grupal_POO/src/Parte_2_Validacion_Metodos/Ejercicio_4/Main.java
+++ Tarea5_Grupal_POO/src/Parte_2_Validacion_Metodos/Ejercicio_4/Main.java
@@ -39,29 +39,3 @@ public class Main {
         sc.close();
     }
 }
-
-//import java.util.Set;
-//
-//public class ConfiguracionUsuario {
-//    private String idioma;
-//    private boolean notificaciones;
-//    private static final Set<String> IDIOMAS_VALIDOS = Set.of("Español", "Inglés", "Francés");
-//
-//    public boolean setIdioma(String idioma) {
-//        if (!IDIOMAS_VALIDOS.contains(idioma)) return false;
-//        this.idioma = idioma;
-//        return true;
-//    }
-//
-//    public void setNotificaciones(boolean estado) {
-//        this.notificaciones = estado;
-//    }
-//
-//    public String getIdioma() {
-//        return idioma;
-//    }
-//
-//    public boolean isNotificacionesActivadas() {
-//        return notificaciones;
-//    }
-//}
