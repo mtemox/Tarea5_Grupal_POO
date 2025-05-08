b5eba0953b4bf76e1bc825153436c37edc6b7ebc
DiegoM-Hack <127579942+DiegoM-Hack@users.noreply.github.com>
2025-05-07 22:12:39 -0500

    Add files via upload

---
+++ Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_3/Descuento.java
@@ -0,0 +1,16 @@
+package Parte_1_Metodos_y_Retorno.Ejercicio_2;
+
+public class Descuento {
+
+    private int total;
+    private int precio;
+
+    public Descuento() {
+
+    }
+    public static String saludarUsuario(String nombre) {
+        System.out.println("Hola, " + nombre + "! Bienvenido/a.");
+        return nombre ;
+    }
+
+}
