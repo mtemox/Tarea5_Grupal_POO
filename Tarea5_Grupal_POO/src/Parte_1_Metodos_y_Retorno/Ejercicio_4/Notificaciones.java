e4489721e828c7165f70c6742ee2cf2c106fec1a
Ejercicio1 <josselyn.ayoharo@epn.edu.ec>
2025-05-07 20:29:45 -0500

    Josselyn Ayo - Ejercicio 4-6

--- Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_4/Notificaciones.java
+++ Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_4/Notificaciones.java
@@ -1,7 +1,7 @@
 package Parte_1_Metodos_y_Retorno.Ejercicio_4;
 
 public class Notificaciones {
-    //Metodo :).
+    //Metodo :)
     public void enviarNotificacion(){
         System.out.println("¡Notificación enviada con éxito!" );
     }
