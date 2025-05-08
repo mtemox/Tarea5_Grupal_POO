2553d0cc052671e91c99d2de9866eef0b21483de
POO <juan.lucero@epn.edu.ec>
2025-05-08 08:06:43 -0500

    commit 1

--- Tarea5_Grupal_POO/src/Parte_2_Validacion_Metodos/Ejercicio_2/Main.java
+++ Tarea5_Grupal_POO/src/Parte_2_Validacion_Metodos/Ejercicio_2/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double velocidad = -1;
+        double velocidad = 0;
 
         // Leer velocidad inicial
         while (true) {
