e05535716cdea2c92ce72a80894471c9cd9d73e4
DiegoM-Hack <127579942+DiegoM-Hack@users.noreply.github.com>
2025-05-07 21:47:39 -0500

    Add files via upload
    1

---
+++ Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_1/Descuento.java
@@ -0,0 +1,20 @@
+package Parte_1_Metodos_y_Retorno.Ejercicio_1;
+
+public class Descuento {
+    private int total;
+    private int precio;
+    public String nombre;
+
+    public Descuento() {
+    }
+
+    public Descuento(  int total, int precio ) {
+        this.total = total;
+        this.precio = precio;
+    }
+
+    public static int calcularDescuento(double precioOriginal, double porcentajeDescuento) {
+        double descuento = precioOriginal * (porcentajeDescuento / 100.0);
+        return (int) descuento;
+    }
+}
