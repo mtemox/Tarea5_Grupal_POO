2ff40784db8014515e9f9ca99df670e132827bf1
DiegoM-Hack <127579942+DiegoM-Hack@users.noreply.github.com>
2025-05-07 21:46:56 -0500

    Update Main.java

--- Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_1/Main.java
+++ Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_1/Main.java
@@ -1,7 +1,19 @@
 package Parte_1_Metodos_y_Retorno.Ejercicio_1;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el total de su compra: ");
+        int precio = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Ingrese su descuento %: ");
+        int descuento = sc.nextInt();
+        sc.nextLine();
+
+        Descuento cliente = new Descuento();
+
+        cliente.calcularDescuento(descuento,precio);
     }
 }
