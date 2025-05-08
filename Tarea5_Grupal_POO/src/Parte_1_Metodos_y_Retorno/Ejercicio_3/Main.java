6dc93fc12892866c2ce454ac3d8532f0dc005b92
DiegoM-Hack <127579942+DiegoM-Hack@users.noreply.github.com>
2025-05-07 22:02:09 -0500

    Update Main.java

--- Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_3/Main.java
+++ Tarea5_Grupal_POO/src/Parte_1_Metodos_y_Retorno/Ejercicio_3/Main.java
@@ -1,7 +1,16 @@
 package Parte_1_Metodos_y_Retorno.Ejercicio_3;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del usuario: ");
+        String usuario = sc.nextLine();
+
+        Descuento cliente = new Descuento();
+        cliente.saludarUsuario(usuario);
+
+        
     }
 }
